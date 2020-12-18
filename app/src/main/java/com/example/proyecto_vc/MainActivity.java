a090d14943ad15e1370d5ad5a04759dc70b34fb5
VictorCastillo02 <55723264+VictorCastillo02@users.noreply.github.com>
2020-12-17 23:41:59 -0500

    COLORES

--- app/src/main/java/com/example/proyecto_vc/MainActivity.java
+++ app/src/main/java/com/example/proyecto_vc/MainActivity.java
@@ -14,75 +14,34 @@ import android.widget.CompoundButton;
 
 public class MainActivity extends AppCompatActivity {
     private CheckBox ck1,ck2, ck3;
-    Button btn;
+    Button btn1, btn2, btn3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ck1 = (CheckBox) findViewById(R.id.ck1);
-        ck2 = (CheckBox) findViewById(R.id.ck2);
-        ck3 = (CheckBox) findViewById(R.id.ck3);
-        btn = findViewById(R.id.btn);
-        ck1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if(b==true && ck2.isChecked()==true){
-                    ck2.toggle();
-                }
-                else if(b==true && ck3.isChecked()==true){
-                    ck3.toggle();
-                }
-                else if(b==true && ck2.isChecked()==true && ck3.isChecked()==true){
-                    ck2.toggle();
-                    ck3.toggle();
-                }
-            }
-        });
-        ck2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if(b==true && ck1.isChecked()==true){
-                    ck1.toggle();
-                }
-                else if(b==true && ck3.isChecked()==true){
-                    ck3.toggle();
-                }
-                else if(b==true && ck1.isChecked()==true && ck3.isChecked()==true){
-                    ck1.toggle();
-                    ck3.toggle();
-                }
-            }
-        });
-        ck3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if(b==true && ck1.isChecked()==true){
-                    ck1.toggle();
-                }
-                else if(b==true && ck2.isChecked()==true){
-                    ck2.toggle();
-                }
-                else if(b==true && ck1.isChecked()==true && ck2.isChecked()==true){
-                    ck1.toggle();
-                    ck2.toggle();
-                }
-            }
-        });
-        btn.setOnClickListener(new View.OnClickListener() {
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn2 = (Button) findViewById(R.id.btn2);
+        btn3 = (Button) findViewById(R.id.btn3);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (ck1.isChecked() == true) {
-                    Intent prox = new Intent(MainActivity.this, Proximidad.class);
-                    startActivityForResult(prox,0);
-                }
-                if (ck2.isChecked()==true){
-                    Intent maps = new Intent(MainActivity.this, MainLocalizacion.class);
-                    startActivityForResult(maps, 0);
-                }
-                if (ck3.isChecked()==true){
-                    Intent acel = new Intent(MainActivity.this, MainAcelerometro.class);
-                    startActivityForResult(acel,0);
-                }
+                Intent prox = new Intent(MainActivity.this, Proximidad.class);
+                startActivityForResult(prox,0);
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent maps = new Intent(MainActivity.this, MainLocalizacion.class);
+                startActivityForResult(maps, 0);
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent acel = new Intent(MainActivity.this, MainAcelerometro.class);
+                startActivityForResult(acel,0);
             }
         });
     }
